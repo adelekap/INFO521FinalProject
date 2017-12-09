fa1d8a22e42f3d759102e6cdab4460778aa79268
Adele Kapellusch <akoutia@email.arizona.edu>
2017-12-08 21:11:55 -0700

    Finished dbscan

--- lasso.py
+++ lasso.py
@@ -13,11 +13,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 xmax = int(max([X_train['Working Memory CIPL'].max(), X_train['Water Maze CIPL'].max()]))
 xmin = int(min([X_train['Working Memory CIPL'].min(), X_train['Water Maze CIPL'].min()]))
 
-reg = linear_model.Lasso(alpha=0.1)
+reg = linear_model.Lasso(alpha=1.0)
 reg.fit(X_train,y_train)
 intercept = reg.intercept_
 coefficients = reg.coef_
 cv = np.mean(cross_val_score(reg,X_test,y_test,cv=10))
-plot_fit_3D(intercept,coefficients,range(xmin-1,xmax+2),X_train,y_train,'Lasso Regression,',
+plot_fit_3D(intercept,coefficients,range(xmin-1,xmax+2),X_train,y_train,'Lasso Regression1.0',
             cv,'Results/Regression/Lasso/')
 
